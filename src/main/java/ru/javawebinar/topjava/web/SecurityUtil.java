b567ffc24b7d867724745e86815b8c51081e211d
p3rsonal <artursspirins@gmail.com>
2022-12-02 20:57:41 +0300

    Add webjar to mvc, change styles

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -1,10 +1,11 @@
 package ru.javawebinar.topjava.web;
 
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
 
-    private static int id = 1;
+    private static int id = START_SEQ;
 
     private SecurityUtil() {
     }
