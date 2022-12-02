a026024f8ccf5ee7041645a622888fa3655e4e93
p3rsonal <artursspirins@gmail.com>
2022-12-02 21:32:02 +0300

    Add AdminUIController and changes in UI

---
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
@@ -0,0 +1,41 @@
+package ru.javawebinar.topjava.web.user;
+
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+
+@RestController
+@RequestMapping(value = "/admin/users", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AdminUIController extends AbstractUserController {
+
+    @Override
+    @GetMapping
+    public List<User> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void create(@RequestParam String name,
+                       @RequestParam String email,
+                       @RequestParam String password) {
+        super.create(new User(null, name, email, password, Role.USER));
+    }
+}
