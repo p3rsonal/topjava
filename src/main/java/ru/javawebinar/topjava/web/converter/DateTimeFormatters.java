90dbab768998e0fbbe55a7ba7bb296e444879aca
p3rsonal <artursspirins@gmail.com>
2022-12-02 19:33:03 +0300

    Add time formatters

---
+++ src/main/java/ru/javawebinar/topjava/web/converter/DateTimeFormatters.java
@@ -0,0 +1,35 @@
+package ru.javawebinar.topjava.web.converter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import org.springframework.format.Formatter;
+
+public class DateTimeFormatters {
+    public static class LocalDateFormatter implements Formatter<LocalDate> {
+
+        @Override
+        public LocalDate parse(String text, Locale locale) {
+            return LocalDate.parse(text);
+        }
+
+        @Override
+        public String print(LocalDate lt, Locale locale) {
+            return lt.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        }
+    }
+
+    public static class LocalTimeFormatter implements Formatter<LocalTime> {
+
+        @Override
+        public LocalTime parse(String text, Locale locale) {
+            return LocalTime.parse(text);
+        }
+
+        @Override
+        public String print(LocalTime lt, Locale locale) {
+            return lt.format(DateTimeFormatter.ISO_LOCAL_TIME);
+        }
+    }
+}
